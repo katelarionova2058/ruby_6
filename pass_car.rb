c18e669d6692343b9e86d42be2c62fdf5fcb9451
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2021-12-19 01:10:15 +0300

    Update pass_car.rb

--- pass_car.rb
+++ pass_car.rb
@@ -1,1 +1,26 @@
+class PassCar < Car
 
+  def initialize(number, place_cnt)
+    @number = number
+    @type = :pass
+    @init_place_cnt = place_cnt
+    @place_cnt = place_cnt
+    validate!
+  end
+
+  def take_place(train_get, number_get)
+    search_of_train = @trains.bsearch { |train| train.name == train_get}
+    search_of_num = search_of_train.cars.bsearch { |car| car.number == number_get }
+    search_of_num.place_cnt = search_of_num.place_cnt - 1 if search_of_num.place_cnt > 0
+  end
+
+  def engaged
+    engaged = @init_place_cnt - @place_cnt
+    return engaged
+  end
+
+  def free
+    return place_cnt
+  end
+  
+end
