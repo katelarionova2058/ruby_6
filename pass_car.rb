9fae4a56b2c106006c6e6ba9034418bef38bbaff
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2021-12-21 00:46:59 +0300

    Update pass_car.rb

--- pass_car.rb
+++ pass_car.rb
@@ -6,9 +6,11 @@ class PassCar < Car
     @place_cnt = place_cnt.to_i
     validate!
   end
+
   def filling_the_car
     place_cnt = place_cnt - 1 if place_cnt > 0
   end
+  
   def engaged
     @init_place_cnt - @place_cnt
   end
