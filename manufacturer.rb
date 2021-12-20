4de5f195bef7986a5ef38b36c302fa8013d01985
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2021-12-20 15:50:19 +0300

    Update manufacturer.rb

--- manufacturer.rb
+++ manufacturer.rb
@@ -1,11 +1,7 @@
 module Manufacturer
-
-  attr_reader :fabrica
-  
+  attr_reader :fabrica  
   def fabricator (manufacture)
     self.fabrica = manufacture
   end
-
   attr_accessor :fabrica
-
 end
