4b307188ffd7715ee2b3e288dab4bbe9904526db
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2021-12-20 15:48:07 +0300

    Update car.rb

--- car.rb
+++ car.rb
@@ -1,24 +1,17 @@
 require_relative 'instance_counter.rb'
 require_relative 'validate.rb'
-
 class Car
-
   include InstanceCounter
   include Validate
-
   attr_accessor :type,
                 :number
-
   def initialize
     @type   = type
   end
-
   protected
-
   def validate!
     raise "Номер вагона не может быть пустым" if number.empty?
     raise "Номер вагона не может быть равен 0" if number == '0'
     true
   end
-
 end
