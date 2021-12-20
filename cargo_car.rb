656385b23a2a01d71db967549619d98ca0ebb71a
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2021-12-20 15:48:30 +0300

    Update cargo_car.rb

--- cargo_car.rb
+++ cargo_car.rb
@@ -1,26 +1,15 @@
 class CargoCar < Car
-
   def initialize(number, volume)
   	@number = number
     @type = :cargo
-    @init_vol = volume
-    @volume = volume
+    @init_vol = volume.to_i
+    @volume = volume.to_i
     validate!
   end
-
-  def take_place(train_get, number_get)
-    search_of_train = @trains.bsearch { |train| train.name == train_get}
-    search_of_num = search_of_train.cars.bsearch { |car| car.number == number_get }
-    search_of_num.volume = search_of_num.volume - 1 if search_of_num.volume > 0
+  def filling_the_car
+    @volume = @volume - 1 if @volume > 0
   end
-
   def engaged
-    engaged = @init_vol - @volume
-    return engaged
-  end
-
-  def free
-    return volume
-  end
-  
+    @init_vol - @volume
+  end 
 end
